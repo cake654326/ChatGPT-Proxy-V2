4eb038a238b4dcb29c78b484230ebb44d7e67bed
Antonio Cheong <acheong@student.dalat.org>
2023-02-14 02:06:02 +0800

    update model name

--- internal/api/openai.go
+++ internal/api/openai.go
@@ -94,7 +94,7 @@ func Send(request types.CompletionRequest, writer gin.ResponseWriter, c *gin.Con
 
 	// Check status code
 	if resp.StatusCode != 200 {
-		c.JSON(resp.StatusCode, gin.H{"error": "OpenAI error"})
+		c.JSON(523, gin.H{"error": "OpenAI error"})
 		return
 	}
 
